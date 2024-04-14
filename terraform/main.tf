5c934038ae047bc3b812ad06dfcfcba027ff446e
alvaro <alvaro.ruiz.1234@outlook.com>
2024-04-14 11:01:52 +0200

    Modify providers & include user-data.sh

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,14 +8,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "3.51.0"
     }
+
     random = {
       source  = "hashicorp/random"
       version = "3.5.1"
     }
   }
-  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
