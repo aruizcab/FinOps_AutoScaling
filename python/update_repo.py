e2740fe5583112717c9b239097ee1970d46515db
alvaro <alvaro.ruiz.1234@outlook.com>
2024-05-25 17:49:46 +0200

    retocar update_repo.py

--- python/update_repo.py
+++ python/update_repo.py
@@ -3,31 +3,36 @@ from github import InputGitTreeElement
 import os
 
 
-# Parameters 
+# parameters 
+action = os.environ["ACTION"]                       # action that will be executed
 # user = os.environ["USERNAME"]                     # github user name
 user = "aruizcab"                                   # github user name
 url = f"https://api.github.com/users/{user}"        # url to request
-password = os.environ["TF_VAR_GH_TOKEN"]               # github token
+password = os.environ["TF_VAR_GH_TOKEN"]            # github token
+# repo = os.environ["REPOSITORY"]                   # repository name
 repo = "FinOps_AutoScaling"                         # repository name
-# repo = os.environ["REPOSITORY"]                     # repository name
 target_branch = "main"                              # branch to update
-file_list = [                                      # list with the paths of the files that will be updated
+file_list = [                                       # list with the paths of the files that will be updated
     "python/config.json",
     "terraform/compute.tf"
 ]
-file_names = [
-    'config.json',
-    'compute.tf'
-]
 
+
+# login to github
 g = Github(user,password)
+# get target repository
 repo = g.get_user().get_repo(repo) # repo name
 
-commit_message = 'auto-update scale-set'
+commit_message = f'auto-update: {action}'
+
+# get target branch reference (main)
 main_ref = repo.get_git_ref('heads/main')
+# get target branch sha (main)
 main_sha = main_ref.object.sha
+# get target branch tree (main)
 base_tree = repo.get_git_tree(main_sha)
 
+# obtain target files tree element
 element_list = list()
 for i, entry in enumerate(file_list):
     with open(entry) as input_file:
@@ -35,7 +40,11 @@ for i, entry in enumerate(file_list):
     element = InputGitTreeElement(file_list[i], '100644', 'blob', data)
     element_list.append(element)
 
+# create git tree
 tree = repo.create_git_tree(element_list, base_tree)
+# obtain git commit
 parent = repo.get_git_commit(main_sha)
+# create new commit with defined tree
 commit = repo.create_git_commit(commit_message, tree, [parent])
+# push new commit to remote
 main_ref.edit(commit.sha)
