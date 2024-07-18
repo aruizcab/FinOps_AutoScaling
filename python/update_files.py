0636726e881de428a8d3b08112da99415192fb38
aruizcab <166910572+aruizcab@users.noreply.github.com>
2024-07-19 00:41:57 +0200

    Update update_files.py

--- python/update_files.py
+++ python/update_files.py
@@ -37,7 +37,7 @@ def obtain_new_config(config, action):
 def generate_compute_file(n_vms_new, sku_new):
     # Generates new content of compute.tf  
     compute_file = f'''
-resource "azurerm_orchestrated_virtual_machine_scale_set" "vmss_terraform_tfm" {
+resource "azurerm_orchestrated_virtual_machine_scale_set" "vmss_terraform_tfm" \{
   name                        = "vmss-terraform"
   resource_group_name         = azurerm_resource_group.rg.name
   location                    = azurerm_resource_group.rg.location
@@ -47,45 +47,45 @@ resource "azurerm_orchestrated_virtual_machine_scale_set" "vmss_terraform_tfm" {
   zones                       = ["1"] # Zones required to lookup zone in the startup script
 
   user_data_base64 = base64encode(file("user-data.sh"))
-  os_profile {
-    linux_configuration {
+  os_profile \{
+    linux_configuration \{
       disable_password_authentication = true
       admin_username                  = "azureuser"
-      admin_ssh_key {
+      admin_ssh_key \{
         username   = "azureuser"
         public_key = var.PUB_KEY
-      }
-    }
-  }
+      \}
+    \}
+  \}
 
-  source_image_reference {
+  source_image_reference \{
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-LTS-gen2"
     version   = "latest"
-  }
-  os_disk {
+  \}
+  os_disk \{
     storage_account_type = "Premium_LRS"
     caching              = "ReadWrite"
-  }
+  \}
 
-  network_interface {
+  network_interface \{
     name                          = "nic"
     primary                       = true
     enable_accelerated_networking = false
 
-    ip_configuration {
+    ip_configuration \{
       name                                   = "ipconfig"
       primary                                = true
       subnet_id                              = azurerm_subnet.subnet.id
       load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bepool.id]
-    }
-  }
+    \}
+  \}
 
-  boot_diagnostics {
+  boot_diagnostics \{
     storage_account_uri = ""
-  }
-}'''
+  \}
+\}'''
     return compute_file
 
 # Update compute.tf
